6f478e8c77a8495f4fd6a44d57098625a00c9c3a
abhi-si <92526811+abhi-si@users.noreply.github.com>
2024-06-06 14:14:15 +0530

    projects added

---
+++ src/Components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/pro1.png"
+import pro2 from "../assets/pro2.jpg"
+import pro3 from "../assets/pro3.jpg"
+
+const ProjectCardData = [
+    {
+        imgSrc: pro1,
+        title: "Netflix-clone",
+        text:"lkshlkjdsgj kfjdjgsdjg ljsdjds",
+        view: "",
+        source:"https://github.com/abhi-si/react_cloneNetflix"
+    },
+    {
+        imgSrc: pro2,
+        title: "Food delivery",
+        text:"lkshlkjdsgj kfjdjgsdjg ljsdjds",
+        view: "",
+        source:"https://github.com/abhi-si/Food-delivery"
+    },
+    {
+        imgSrc: pro3,
+        title: "Coming soon",
+        text:"lkshlkjdsgj kfjdjgsdjg ljsdjds",
+        view: "",
+        source:""
+    }
+]
+
+export default ProjectCardData
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import ProjectHome from '../Components/ProjectHome'
+import Work from '../Components/Work'
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <ProjectHome heading="PROJECTS" text="Some of my recent work"/>
+      <Work/>
       <Footer/>
     </div>
   )
