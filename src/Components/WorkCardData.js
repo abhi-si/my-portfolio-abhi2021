4130884fd9041e56f92093ef4ce177efa86904f9
abhi-si <92526811+abhi-si@users.noreply.github.com>
2024-10-24 01:01:18 +0530

    1 project updated

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -3,27 +3,27 @@ import pro2 from "../assets/proj2.jpg"
 import pro3 from "../assets/pro3.jpg"
 
 const ProjectCardData = [
-    {
-        imgSrc: pro1,
-        title: "Netflix-clone",
-        text:"Discover a vast library of movies and TV shows with our Netflix-clone app, designed to provide an immersive and personalized streaming experience. Utilizing the TMDB API, our app offers comprehensive information on the latest releases, popular titles, and hidden gems. With Firebase authentication, users can enjoy a secure and seamless login process, ensuring a personalized and safe viewing experience.",
-        view: "https://myreactproject-9a56a.web.app/",
-        source:"https://github.com/abhi-si/react_cloneNetflix"
-    },
-    {
-        imgSrc: pro2,
-        title: "Food delivery",
-        text:"Explore a diverse array of restaurants offering various cuisines through our innovative food delivery app. Leveraging the power of the live Swiggy API, our app provides real-time data on restaurant options, ensuring you have access to the latest menus, deals, and availability. Whether you're craving local delicacies or international flavors, our platform connects you with the best dining experiences around.",
-        view: "",
-        source:"https://github.com/abhi-si/Food-delivery"
-    },
-    {
-        imgSrc: pro3,
-        title: "Coming soon",
-        text:"lkshlkjdsgj kfjdjgsdjg ljsdjds",
-        view: "",
-        source:""
-    }
-]
+  {
+    imgSrc: pro1,
+    title: "Netflix-clone",
+    text: "Discover a vast library of movies and TV shows with our Netflix-clone app, designed to provide an immersive and personalized streaming experience. Utilizing the TMDB API, our app offers comprehensive information on the latest releases, popular titles, and hidden gems. With Firebase authentication, users can enjoy a secure and seamless login process, ensuring a personalized and safe viewing experience.",
+    view: "https://myreactproject-9a56a.web.app/",
+    source: "https://github.com/abhi-si/react_cloneNetflix",
+  },
+  {
+    imgSrc: pro2,
+    title: "ExpressFood",
+    text: "Explore a diverse array of restaurants offering various cuisines through our innovative food delivery app. Leveraging the power of the live Swiggy API, our app provides real-time data on restaurant options, ensuring you have access to the latest menus, deals, and availability. Whether you're craving local delicacies or international flavors, our platform connects you with the best dining experiences around.",
+    view: "https://food-express-delivery.vercel.app/",
+    source: "https://github.com/abhi-si/food-expressDelivery",
+  },
+  {
+    imgSrc: pro3,
+    title: "Coming soon",
+    text: "working on it",
+    view: "",
+    source: "",
+  },
+];
 
 export default ProjectCardData
